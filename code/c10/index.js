40c66c094e41aaa579371200ec4e6c86416d9a19
Krit Ratchawong <joosaeheuk@hotmail.com>
2019-02-09 17:08:57 +0700

    Update index.js

--- code/c10/index.js
+++ code/c10/index.js
@@ -6,13 +6,100 @@ var app = express();
 app.use(express.static('public'));
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+  extended: false
 })); 
 
 
+var pool = mysql.createPool({
+   host     : 'localhost',
+   user     : 'root',
+   password : '',
+   database : 'nodejs_training'
+ });
+
+
 app.get('/', function (req, res) {
-    res.send('Hello APIs.');
+  
+    res.send('{ data : "kmitl" }');
 })
+
+app.get('/get_topics',function(req, res){
+
+   pool.getConnection(function(err, con){
+      con.query("SELECT * FROM t_topic", function (err, result, fields) {
+         if (err) throw err;
+        
+         con.release();
+         res.send(JSON.stringify(result));
+       });
+   }); 
+});
+
+app.get('/get_topic/:id', function(req,res){
+   pool.getConnection(function(err, con){
+      con.query("SELECT * FROM t_topic where topic_id = ?",[req.params.id], function (err, result, fields) {
+         if (err) throw err;
+        
+         con.release();
+         res.send(JSON.stringify(result));
+       });
+   });
+
+});
+
+app.get('/get_reply/:topic_id', function(req, res){
+   pool.getConnection(function(err, con){
+      con.query("SELECT * FROM t_reply where topic_id = ?",[req.params.topic_id], function (err, result, fields) {
+         if (err) throw err;
+        
+         con.release();
+         res.send(JSON.stringify(result));
+       });
+   });
+
+});
+
+app.get('/get_detail_topic/:topic_id', function(req, res){
+
+   //var response = [];
+   pool.getConnection(function(err, con){
+      con.query("SELECT * FROM t_topic where topic_id = ? ",[req.params.topic_id], function (err, result, fields) {
+         if (err) throw err;
+     
+         con.query("select * from t_reply where topic_id = ? ",[result[0].topic_id],(err, result_reply, fields) => {
+            result[0].replys = result_reply;
+            con.release();
+            res.send(JSON.stringify(result));
+         });
+            
+       });
+   });
+
+});
+
+
+app.post('/create_topic', function(req, res){
+   
+   console.log(req.body);
+   res.send("");
+
+   /*
+   pool.getConnection(function(err, con){
+      con.query("INSERT INTO t_topic(topic_id, topic_name,created_date)VALUES(null,?,NOW())",[req.query.topic_name], function (err, result, fields) {
+         if (err) throw err;
+         con.release();
+         res.send({ 'status' : 'Create Success'});
+       });
+   });*/
+
+   
+
+});
+
+
+
+
+
 
  var server = app.listen(8081, function () {
     var host = server.address().address
